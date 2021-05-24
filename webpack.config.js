1595552acdaa0035863b4df0fc23aab753fdfad0
Obidi <obidi.isaacw@gmail.com>
2021-05-24 07:45:35 +0100

    Completed github user repository page

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: ['@babel/polyfill', './src/index.js'],
+    // app: ['@babel/polyfill', './src/index.js'],
+    app: glob.sync('./src/**/*.js'),
   },
+  mode: 'production',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.bundle.js',
@@ -14,12 +22,18 @@ module.exports = {
       {
         test: /\.es6$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        loader: 'babel-loader',
         query: {
           presets: ['@babel/preset-env'],
         },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        use: ['url-loader'],
       },
     ],
   },
@@ -30,5 +44,59 @@ module.exports = {
         token: process.env.token,
       },
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new CleanWebpackPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new OptimizeCssAssetsPlugin()],
+  },
 };
+
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   mode: 'production',
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     filename: 'bundle.js',
+//   },
+//   plugins: [new MiniCssExtractPlugin()],
+//   module: {
+//     rules: [
+// {
+//   test: /\.css$/,
+//   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+// },
+// {
+//   test: /\.(svg|gif|png|eot|woff|ttf)$/,
+//   use: ['url-loader'],
+// },
+//       {
+//         test: /\.js$/,
+//         exclude: /(node_modules|bower_components)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//           },
+//         },
+//       },
+//     ],
+//   },
+// optimization: {
+//   minimize: true,
+//   minimizer: [
+//     // new TerserPlugin({
+//     //   extractComments: false,
+//     // }),
+//     new OptimizeCssAssetsPlugin(),
+//   ],
+// },
+// };
